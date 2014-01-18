a3f9365fc1dbc422ea3004d51145bcddd903f140
Priyanka <priyanka.bhalerao@gmail.com>
2014-01-18 09:04:25 -0800

    Initial Commit

---
+++ ToDoApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoApp
+//
+//  Created by Priyanka Bhalerao on 1/18/14.
+//  Copyright (c) 2014 iOSClass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
